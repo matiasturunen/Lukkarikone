fb08b0bcfe2ec989ae68760d2881f5877ceeb185
Matias Turunen <mt95@windowslive.com>
2015-09-26 23:45:25 +0300

    moved application logic to main file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Lukkarikone
+
+This is a hobby project to test out various things including parsing things out of a HTML page. 
+
+Also there might be some school schelude creating automation happening.
+
+This project is heavily dependent of Lappeenranta University of Technology's UNI-web portal
--- main.py
+++ main.py
@@ -7,11 +7,28 @@
 # Created:     25.09.2015
 # Copyright:   (c) Matias 2015
 # ------------------------------------------------------------------------------
+from lib.utilities import links, html
+from lib.utilities import debug
+from lib.models import Link
 
+
+uniURL = "https://uni.lut.fi"
+scheludeListURL = uniURL + "/fi/lukujarjestykset1"
 
 def main():
-    print("asd")
-    pass
+    # get links to schelude pages
+    linklist = links.getScheludeLinks(scheludeListURL)
+    debug.printList(linklist)
+
+    for item in linklist:
+        scheludePage = html.getHTML(uniURL + item.url)
+        if( not scheludePage):
+            print("Skipping invalid page")
+            continue
+        print(item.name)
+        print(scheludePage)
+        print("")
+
 
 if __name__ == '__main__':
     main()
