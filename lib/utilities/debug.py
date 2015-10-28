02af9f7502d4cb5b1b3f44feb33b22c37a4d3be4
Matias Turunen <mt95@windowslive.com>
2015-10-28 20:57:09 +0200

    Added support for nested lists

--- lib/utilities/debug.py
+++ lib/utilities/debug.py
@@ -9,18 +9,22 @@
 # ------------------------------------------------------------------------------
 
 
-def printList(listToPrint, emptyline=True):
+def printList(listToPrint, emptyline=True, depth=0):
     """Print given list to console.
 
         Keyword arguments:
         listToPrint -- List to be printed
         emptyline -- Will there be empty line after each item in list
+        depth -- internal argument to make printing nested lists more readable
     """
     for item in listToPrint:
         try:
-            print(item)
-            if emptyline:
-                print("")
+            if isinstance(item, list):
+                printList(item, emptyline, depth + 1)
+            else:
+                print('    '*depth + str(item))
+                if emptyline:
+                    print("")
         except UnicodeError as e:
             print("Skipping print due error", e)
         
