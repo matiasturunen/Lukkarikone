d8b33e481a00d68d1fe4665a52f87cb4040cdb59
Matias Turunen <mt95@windowslive.com>
2015-12-09 10:05:27 +0000

    Changed urls so that root url points to search page

--- lukkarikone/main/urls.py
+++ lukkarikone/main/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.indexView, name='index'),
+    url(r'^$', views.searchSimpleScheludes, name='index'),
     url(r'^search$', views.searchSimpleScheludes, name='search'),
 ]
