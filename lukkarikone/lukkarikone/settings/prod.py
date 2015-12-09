fdf117d6ca7df7a1228c5a410f338e0d1babb509
Matias Turunen <mt95@windowslive.com>
2015-12-09 12:05:45 +0000

    More settings for production

--- lukkarikone/lukkarikone/settings/prod.py
+++ lukkarikone/lukkarikone/settings/prod.py
@@ -7,9 +7,17 @@ from .common import *
 # Common production settings
 DEBUG = False
 
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True          
+SESSION_COOKIE_SECURE = True       
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
 X_FRAME_OPTIONS = 'DENY'
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False         # Needs SSL
+CSRF_COOKIE_HTTPONLY = True
+
+# this may need some work
+ALLOWED_HOSTS = [".matiasturunen.koding.io", ""]
+
+# static files location
+STATIC_ROOT = "/home/matiasturunen/Web/lukkarikone-files"
+STATIC_URL = 'http://umkk434cba97.matiasturunen.koding.io/lukkarikone-files/'
