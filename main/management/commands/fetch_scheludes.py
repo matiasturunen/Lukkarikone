7a60560d20575497ebf3d113ee2430280f6c4a60
Matias Turunen <mt95@windowslive.com>
2015-12-10 15:14:16 +0000

    Added transaction to greatly boost performance (2.5minutes --> 20seconds)

--- main/management/commands/fetch_scheludes.py
+++ main/management/commands/fetch_scheludes.py
@@ -1,23 +1,18 @@
 from django.core.management.base import BaseCommand, CommandError
 from main.parser import parser
 from main.models import Schelude
+from django.db import transaction
 
 class Command(BaseCommand):
     help = 'Fetches scheludes from internet and deletes existing ones'
     
+    @transaction.atomic
     def handle(self, *args, **options):
         print("Hangling...")
         # flush all tables related to schelude
         Schelude.objects.all().delete()
         
         # get new scheludes
-        times = 10
-        x = 1
-        while x < times:
-            print("Attempt", x)
-            x += 1
-            ok = parser.fetchScheludes()
-            if ((x == times) or (ok == True)):
-                break
-            
+        ok = parser.fetchScheludes()
+
         self.stdout.write('Hangling done. Success: {0}'.format(ok))
