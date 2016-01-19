6aa3e3feeeef4a6b0fdb773d9636192a92fb30c6
Matias Turunen <mt95@windowslive.com>
2016-01-19 11:09:05 +0000

    Deployment schenanigas continued p2

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -6,6 +6,6 @@ call(["python", "manage.py", "migrate"])
 call(["python", "manage.py", "loaddata",  "main/fixtures/*"])
 call(["python", "manage.py", "fetch_scheludes"])
 
-bind = ["127.0.0.1:8000", "0.0.0.0:8080"]
+bind = "0.0.0.0:8000"
 
 
