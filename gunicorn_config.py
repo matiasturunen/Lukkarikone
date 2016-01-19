a42fa7c6e26b824c04aaf9a3adacc92b3f01e238
Matias Turunen <mt95@windowslive.com>
2016-01-19 17:11:55 +0000

    Added PORT variable to gunicorn configs

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,11 +1,12 @@
 # gunicorn config
 
+import os
 from subprocess import call
 
 call(["python", "manage.py", "migrate"])
 call(["python", "manage.py", "loaddata",  "main/fixtures/*"])
 call(["python", "manage.py", "fetch_scheludes"])
 
-bind = "0.0.0.0:8000"
+bind = "0.0.0.0:" + os.environ.get("PORT", "8000")
 
 
