4a34846e4064eee48df1ec418d5f0bd627268a9f
Matias Turunen <mt95@windowslive.com>
2016-02-08 12:18:49 +0000

    Removed parser commands from boot script

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -3,10 +3,11 @@
 import os
 from subprocess import call
 
+""" These need to be run manually on deploy
 call(["python", "manage.py", "migrate"])
 call(["python", "manage.py", "loaddata",  "main/fixtures/*"])
 call(["python", "manage.py", "fetch_scheludes"])
-
+"""
 bind = "0.0.0.0:" + os.environ.get("PORT", "8000")
 
 
